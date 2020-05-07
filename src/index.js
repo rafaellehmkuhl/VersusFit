5befc5f2eede5b08a1e0ac1505fa59a02aa9dac8
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2020-05-07 20:38:13 -0300

    Add Redux boilerplate

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from "./components/App";
+import reducers from "./reducers";
+
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
