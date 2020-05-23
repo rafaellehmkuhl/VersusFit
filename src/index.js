fdb9269156b66ec57ad206fcfbb6f3db4d84fa53
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2020-05-23 19:05:13 -0300

    Add redux-dev-tools ; Remove react-router and redux-logger

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import { createLogger } from "redux-logger";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
-const logger = createLogger();
-const store = createStore(reducers, applyMiddleware(thunk, logger));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
