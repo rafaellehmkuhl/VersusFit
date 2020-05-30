c4c1393b08bfef46ac6cb5f89b5af01a3da0985a
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2020-05-30 15:34:11 -0300

    Refactor goal reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,9 +4,10 @@ import authReducer from "./authReducer";
 const goalsReducer = (state = {}, action) => {
   switch (action.type) {
     case "FETCH_GOALS":
+      const challengerName = action.challengerName;
       const newState = Object.assign({}, state);
-      delete newState.challengerName;
-      return Object.assign(newState, action.payload);
+      delete newState[challengerName];
+      return Object.assign(newState, { [challengerName]: action.payload });
 
     case "DELETE_GOAL":
       console.log("deletando");
