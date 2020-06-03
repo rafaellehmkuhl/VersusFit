3ca1a16575512fe0d141f2b2a159fefa45eacea6
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2020-06-03 15:24:26 -0300

    Change Object.assign for new spread operator

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,14 +4,11 @@ import authReducer from "./authReducer";
 const goalsReducer = (state = {}, action) => {
   switch (action.type) {
     case "FETCH_GOALS":
-      const user_id = action.user_id;
-      const newState = Object.assign({}, state);
-      delete newState[user_id];
-      return Object.assign(newState, { [user_id]: action.payload });
+      return { ...state, [action.user_id]: action.payload };
 
     case "DELETE_GOAL":
       console.log("deletando");
-      return Object.assign({}, action.payload);
+      return { ...action.payload };
 
     default:
       return state;
