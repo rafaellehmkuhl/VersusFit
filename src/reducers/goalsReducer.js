586eeb0aeaeae98b85b5a611b311e81f41a2b72f
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2020-06-03 23:55:17 -0300

    Implement new action types and respective reducers
    New structure uses unique goals object (not an array anymore) for both users.
    New actions prevent fetching all the goals when an add, update or delete action is dispatched.

--- src/reducers/goalsReducer.js
+++ src/reducers/goalsReducer.js
@@ -1,9 +1,21 @@
-import { FETCH_USER_GOALS } from "../actions/types";
+import {
+  FETCH_USER_GOALS,
+  ADD_USER_GOAL,
+  UPDATE_USER_GOAL,
+  DELETE_USER_GOAL,
+} from "../actions/types";
+import _ from "lodash";
 
 const goalsReducer = (state = {}, action) => {
   switch (action.type) {
     case FETCH_USER_GOALS:
-      return { ...state, [action.user_id]: action.payload };
+      return { ...state, ..._.mapKeys(action.payload, "id") };
+    case ADD_USER_GOAL:
+      return { ...state, [action.payload.id]: action.payload };
+    case UPDATE_USER_GOAL:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_USER_GOAL:
+      return _.omit(state, action.payload.id);
 
     default:
       return state;
