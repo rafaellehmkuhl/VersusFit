00ee66c22384cedb71917c19b8e2e450d53e515e
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2020-06-03 23:51:16 -0300

    Add new action types

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,6 @@
 export const SIGN_IN = "SIGN_IN";
 export const SIGN_OUT = "SIGN_OUT";
 export const FETCH_USER_GOALS = "FETCH_USER_GOALS";
+export const ADD_USER_GOAL = "ADD_USER_GOAL";
+export const UPDATE_USER_GOAL = "UPDATE_USER_GOAL";
+export const DELETE_USER_GOAL = "DELETE_USER_GOAL";
