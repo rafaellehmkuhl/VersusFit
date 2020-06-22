9bb96c7c8d5e94bf9cf43058508b4b4e81d9c5af
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2020-06-22 12:00:24 -0300

    Fix ids
    Que time users are from 7 to 13

--- src/components/ChallengeTime.js
+++ src/components/ChallengeTime.js
@@ -2,22 +2,19 @@ import React, { Component } from "react";
 import { Grid } from "semantic-ui-react";
 import CardCompetidor from "./CardCompetidor";
 
-const user6_id = 6;
 const user7_id = 7;
 const user8_id = 8;
 const user9_id = 9;
 const user10_id = 10;
 const user11_id = 11;
 const user12_id = 12;
+const user13_id = 13;
 
 export default class ChallengeTable extends Component {
   render() {
     return (
       <div>
         <Grid rows={2} centered divided>
-          <Grid.Row>
-            <CardCompetidor user_id={user6_id} />
-          </Grid.Row>
           <Grid.Row>
             <CardCompetidor user_id={user7_id} />
           </Grid.Row>
@@ -35,6 +32,9 @@ export default class ChallengeTable extends Component {
           </Grid.Row>
           <Grid.Row>
             <CardCompetidor user_id={user12_id} />
+          </Grid.Row>
+          <Grid.Row>
+            <CardCompetidor user_id={user13_id} />
           </Grid.Row>
         </Grid>
       </div>
