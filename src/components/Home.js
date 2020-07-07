052bcbf63599f1df3adc2ad1baf83c05758abc8e
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2020-07-07 10:13:00 -0300

    Add instruction content to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Container } from "semantic-ui-react";
 import ChallengeStack from "./ChallengesStack";
 
 class Home extends Component {
@@ -7,7 +8,12 @@ class Home extends Component {
     if (this.props.activeChallenge) {
       return <ChallengeStack challenge={this.props.activeChallenge} />;
     } else {
-      return;
+      return (
+        <Container style={{ paddingTop: "5em", paddingBottom: "5em" }} text>
+          Pick a challenge at the top or create a new challenge with your
+          friends!
+        </Container>
+      );
     }
   }
   render() {
